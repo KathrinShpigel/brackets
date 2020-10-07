5fb6dff0c169a8054c8c10cc5b764711de368412
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2020-10-07 23:46:04 +0300

    feat: add brackets solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = str.split("");
+  const config = {};
+  const strBrackets = [];
+
+  bracketsConfig.forEach(el => config[el[0]] = el[1]);
+
+  brackets.forEach(el => {
+    return (el === (strBrackets[strBrackets.length-1] || 0)) ?
+      strBrackets.pop() :
+      strBrackets.push(config[el]);
+  });
+
+  return !strBrackets.length ? true : false;
 }
